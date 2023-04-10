2eed3c0a03a6ccb68a042c22cd50aba323ccf8a7
Tinymemo <tinymemo@tinkink.net>
2023-04-10 12:19:47 +0800

    feat: add google adsense

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -29,6 +29,7 @@ module.exports = {
     },
     head: [
         ['link', { rel: 'icon', href: '/assets/logo.png' }],
+        ['script', { async: true, src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3100848271969177', crossorigin: 'anonymous' }],
         // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         // ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         // ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
