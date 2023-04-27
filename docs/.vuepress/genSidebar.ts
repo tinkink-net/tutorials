656d144a1b6d7479304aa0f246a93b5f73d595ee
Tinymemo <tinymemo@tinkink.net>
2023-04-27 13:11:09 +0800

    chore: support multi-level directories

--- docs/.vuepress/genSidebar.ts
+++ docs/.vuepress/genSidebar.ts
@@ -1,36 +1,52 @@
 import path from 'path';
 import fs from 'fs';
 
-export default function (subPath: string = '') {
+const rootPath = path.resolve(__dirname, '../');
 
-    const docPath = path.resolve(__dirname, '../', subPath);
-    const folders = fs.readdirSync(docPath);
+type GroupItem = string | Group;
 
-    const ret = [] as any[];
+interface Group {
+    text: string;
+    collapsable: boolean;
+    children: GroupItem[];
+};
 
-    if (folders.indexOf('README.md') > -1) {
-        // ret.push('/');
-    }
 
-    folders.filter((folder) => /^[^.]/.test(folder)).forEach((folder) => {
-        if (folder === 'README.md') {
+const genGroup = function (subPath: string = ''): GroupItem[] {
+
+    const docPath = path.join(rootPath, subPath);
+    const children = fs.readdirSync(docPath);
+
+    const ret: GroupItem[] = [];
+
+    children.filter((childPath) => /^[^.]/.test(childPath)).forEach((childPath) => {
+        if (childPath === 'README.md') {
             return;
         }
-        const mdPath = path.resolve(docPath, folder);
-        const mdFiles = fs.readdirSync(mdPath).filter((file) => /\.md$/.test(file));
-        if (!mdFiles.length) return;
-        const group = {
-            text: folder.replace(/^\d+\./, ''),
-            collapsable: false,
-            children: [] as string[],
-        };
-        group.children = mdFiles.filter((mdFile) => mdFile !== 'README.md').map((mdFile) => {
-            return '/' + subPath + '/' + folder + '/' + mdFile;
-        });
-        ret.push(group);
+
+        const childFullPath = path.resolve(docPath, childPath);
+
+        const isDirectory = fs.statSync(childFullPath).isDirectory();
+
+        if (isDirectory) {
+            // if is directory, add a group and call genGroup recursively
+            const children = genGroup(subPath + '/' + childPath);
+            const group: Group = {
+                text: childPath.replace(/^\d+\./, ''),
+                collapsable: false,
+                children,
+            };
+            ret.push(group);
+        } else {
+            // if is file, add a link
+            ret.push('/' + subPath + '/' + childPath);
+        }
+
     });
 
     // console.log(ret);
 
     return ret;
 };
+
+export default genGroup;
