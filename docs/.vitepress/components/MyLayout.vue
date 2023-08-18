c84e2b24b7bddd0acce3e91506476d8b41844d11
Tinymemo <tinymemo@tinkink.net>
2023-08-18 16:53:35 +0800

    feat: fix ad position

---
+++ docs/.vitepress/components/MyLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <Layout>
+        <template #aside-outline-after>
+            <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-3100848271969177"
+         data-ad-slot="6164958912"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+        </template>
+    </Layout>
+</template>
+
+<script setup>
+import DefaultTheme from 'vitepress/theme';
+const { Layout } = DefaultTheme;
+
+(window.adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<style scoped>
+.adsbygoogle {
+    margin-top: 20px;
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,10 @@
 import DefaultTheme from 'vitepress/theme';
+import MyLayout from '../components/MyLayout.vue';
 import Validator from '../components/Validator.vue';
 
 export default {
+    ...DefaultTheme,
+    Layout: MyLayout,
     extends: DefaultTheme,
     enhanceApp({ app }) {
         app.component('Validator', Validator);
