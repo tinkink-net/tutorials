450e7fcf71c48b08782afe38828f56992662673a
Tinymemo <tinymemo@tinkink.net>
2023-08-21 10:45:19 +0800

    feat: make google adsense client only

---
+++ docs/.vitepress/components/AdSense.vue
@@ -0,0 +1,24 @@
+<template>
+    <ins class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-3100848271969177"
+    data-ad-slot="6164958912"
+    data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+onMounted(() => {
+    if (!window.adsbygoogle) {
+        window.adsbygoogle = [];
+    }
+    window.adsbygoogle.push({});
+});
+</script>
+
+<style scoped>
+.adsbygoogle {
+    margin-top: 20px;
+}
+</style>
--- docs/.vitepress/components/MyLayout.vue
+++ docs/.vitepress/components/MyLayout.vue
@@ -1,27 +1,19 @@
 <template>
     <Layout>
         <template #aside-outline-after>
-            <ClientOnly>
-                <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-client="ca-pub-3100848271969177"
-                data-ad-slot="6164958912"
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-            </ClientOnly>
+            <AdSense />
         </template>
     </Layout>
 </template>
 
 <script setup>
+import { defineClientComponent } from 'vitepress'
 import DefaultTheme from 'vitepress/theme';
+
+const AdSense = defineClientComponent(() => {
+    return import('./AdSense.vue');
+})
+
+
 const { Layout } = DefaultTheme;
-
-(window.adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-
-<style scoped>
-.adsbygoogle {
-    margin-top: 20px;
-}
-</style>
